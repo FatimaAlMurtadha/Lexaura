329b422084b7989fdc3e58227dc0e1ffbd672a7c
FatimaAlmurtadha <sukaina77150@gmail.com>
2026-07-03 15:30:14 +0200

    Query endpoint

--- backend/internal/shared/clients/ai_service_client.go
+++ backend/internal/shared/clients/ai_service_client.go
@@ -7,58 +7,57 @@ import (
 	"mime/multipart"
 	"net/http"
 	"strings"
-    "github.com/FatimaAlMurtadha/Lexaura/backend/internal/shared/clients"
 )
 
 func SendToAIService(file io.Reader, filename string) (string, error) {
-    body := &bytes.Buffer{}
-    writer := multipart.NewWriter(body)
+	body := &bytes.Buffer{}
+	writer := multipart.NewWriter(body)
 
-    part, err := writer.CreateFormFile("file", filename)
-    if err != nil {
-        return "", err
-    }
+	part, err := writer.CreateFormFile("file", filename)
+	if err != nil {
+		return "", err
+	}
 
-    io.Copy(part, file)
-    writer.Close()
+	io.Copy(part, file)
+	writer.Close()
 
-    req, err := http.NewRequest("POST", "http://localhost:8000/api/ingest", body)
-    if err != nil {
-        return "", err
-    }
+	req, err := http.NewRequest("POST", "http://localhost:8000/api/ingest", body)
+	if err != nil {
+		return "", err
+	}
 
-    req.Header.Set("Content-Type", writer.FormDataContentType())
+	req.Header.Set("Content-Type", writer.FormDataContentType())
 
-    client := &http.Client{}
-    resp, err := client.Do(req)
-    if err != nil {
-        return "", err
-    }
-    defer resp.Body.Close()
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
 
-    respBody, _ := io.ReadAll(resp.Body)
-    return string(respBody), nil
+	respBody, _ := io.ReadAll(resp.Body)
+	return string(respBody), nil
 }
 
-func SendQueryToAIService(question string) (string, error){
-    payload := strings.NewReader(fmt.Sprintf(`{"question":"%s"}`, question))
+func SendQueryToAIService(question string) (string, error) {
+	payload := strings.NewReader(fmt.Sprintf(`{"question":"%s"}`, question))
 
-    req , err := http.NewRequest("POST", "http://localhost:8000/api/query", payload)
+	req, err := http.NewRequest("POST", "http://localhost:8000/api/query", payload)
 
-    if err != nil {
-        return "", err
-    }
+	if err != nil {
+		return "", err
+	}
 
-    req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Content-Type", "application/json")
 
-    client := &http.Client{}
-    resp, err := client.Do(req)
-    if err != nil {
-        return "", err
-    }
-    defer resp.Body.Close()
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
 
-    respBody, _ := io.ReadAll(resp.Body)
-    return string(respBody), nil
+	respBody, _ := io.ReadAll(resp.Body)
+	return string(respBody), nil
 
 }
