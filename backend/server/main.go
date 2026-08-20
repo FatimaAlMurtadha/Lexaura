c81305cd3178637b63d6a3801504471dcdf6fb80
FatimaAlmurtadha <sukaina77150@gmail.com>
2026-08-20 09:47:53 +0200

    Testing push

--- backend/server/main.go
+++ backend/server/main.go
@@ -8,6 +8,25 @@ import (
 	"github.com/FatimaAlMurtadha/Lexaura/backend/internal/api"
 )
 
+func corsMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		origin := r.Header.Get("Origin")
+		if origin != "" {
+			w.Header().Set("Access-Control-Allow-Origin", origin)
+			w.Header().Set("Access-Control-Allow-Credentials", "true")
+			w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+			w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		}
+
+		if r.Method == http.MethodOptions {
+			w.WriteHeader(http.StatusNoContent)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
+
 func main() {
 	port := os.Getenv("PORT")
 	if port == "" {
@@ -28,7 +47,7 @@ func main() {
 	addr := ":" + port
 	log.Printf("Backend server listening on %s", addr)
 
-	if err := http.ListenAndServe(addr, mux); err != nil {
+	if err := http.ListenAndServe(addr, corsMiddleware(mux)); err != nil {
 		log.Fatal(err)
 	}
 }
